0c33148f6d3918248557a0662f5f06eac60e5052
trisskmasarahh <trisukmasarahh@gmail.com>
2025-02-25 08:02:47 +0700

    tugas praktikum_js2

---
+++ resources/views/pos/products.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Daftar Produk - POS</title>
+    </head>
+    <body>
+        <h1>Daftar Produk</h1>
+        <ul>
+        <li><a href="{{ url('/category/food-beverage') }}">Kategori Makanan & Minuman</a></li>
+        <li><a href="{{ url('/category/beauty-health') }}">Kategori Kecantikan & Kesehatan</a></li>
+        <li><a href="{{ url('/category/home-care') }}">Kategori Perawatan Rumah</a></li>
+        <li><a href="{{ url('/category/baby-kid') }}">Kategori Bayi & Anak-anak</a></li>
+        </ul>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\POSController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [POSController::class, 'home']);
+
+Route::get('/products', [POSController::class, 'products']);
+
+Route::get('/category/{category}', [POSController::class, 'productsCategory']);
+
+Route::get('/user/{id}/{name}', [POSController::class, 'user']);
+
+Route::get('/sales', [POSController::class, 'sales']);
+
+Route::post('/processSale', [POSController::class, 'processSale'])->name('process.sale');
+
+
